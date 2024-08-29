dfadbc072487537e25194e47eaf5dab789282844
Nino Parisi <aparisi@esw.com>
2024-08-29 15:55:52 +0100

    Adde UT

--- src/Tests/Horizon.Infrastructure.Unit.Tests/SubscriptionsStoreTests.cs
+++ src/Tests/Horizon.Infrastructure.Unit.Tests/SubscriptionsStoreTests.cs
@@ -1,5 +1,7 @@
+using ErrorOr;
 using FluentAssertions;
 using Horizon.Application;
+using Microsoft.AspNetCore.Http;
 using Xunit;
 
 namespace Horizon.Infrastructure.Unit.Tests;
@@ -40,5 +42,23 @@ public class SubscriptionsStoreTests
         result.IsError.Should().BeFalse();
         _sut.GetSubscription(azureKeyVaultName).Value.Should().HaveCount(2);
         _sut.GetSubscription(azureKeyVaultName).Value.Should().OnlyContain(x => x == kubernetesBundle1 || x == kubernetesBundle2);
+    }
+
+    [Fact]
+    public void AddSubscription_WhenNotFound_ShouldReturnError()
+    {
+        // Arrange
+        var azureKeyVaultName = "testKeyVault";
+        var kubernetesBundle1 = new KubernetesBundle("k8sSecretName1", null, "namespace1");
+        _sut.AddSubscription(azureKeyVaultName, kubernetesBundle1);
+
+        // Act
+        var result = _sut.GetSubscription("notfound");
+
+        // Assert
+        // Assert
+        result.IsError.Should().BeTrue();
+        result.FirstError.Should().BeOfType<Error>()
+            .Which.Type.Should().Be(ErrorType.NotFound);
     }
 }
